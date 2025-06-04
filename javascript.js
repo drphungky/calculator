ec184def23ca77644f894550d9cae965d20f6dd3
Bergmann, Erik - BLS <bergmann.erik@bls.gov>
2025-06-04 13:30:57 -0400

    Add sending numbers and decimal
    Added functions to add numbers and decimals to display.

--- javascript.js
+++ javascript.js
@@ -34,8 +34,30 @@ function operate(num1, num2, operator){
 
 
 
-function update_display(){
-    
+function update_display(user_input){
+    if (typeof(user_input) == "number") {
+        send_additional_number_to_display(user_input)
+    }
+    else if (user_input == "."){
+        send_decimal_to_display();
+    }
+}
+
+function send_additional_number_to_display(num){
+    if (display.textContent==0) {
+        display.textContent = num;
+    }
+    else {
+        if (display.textContent.length < 9){
+            display.textContent += num;
+        }
+    }
+}
+
+function send_decimal_to_display(){
+    if (display.textContent.search(/\./) == -1){
+        display.textContent += ".";
+    }
 }
 
 function clear_display(){
