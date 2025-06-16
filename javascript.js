58069fe8741b4366bddd439f9b1e5070709cd9c8
Bergmann, Erik - BLS <bergmann.erik@bls.gov>
2025-06-16 10:07:53 -0400

    Revert "Add equals capability"
    This reverts commit 818fa0f8bcc3b5f18e4d0461b6d3bf14c3c7ca98

--- javascript.js
+++ javascript.js
@@ -1,10 +1,6 @@
 // Globals
 var body = document.querySelector("body");
 var display = document.querySelector("#display")
-var number1 = NaN;
-var number2 = NaN;
-var operator = "add";
-var clear_display_on_next_button_pushed = 0
 
 // Attach Number functions to buttons
 var buttons = document.querySelectorAll("button")
@@ -34,13 +30,13 @@ function divide(num1, num2){
 
 function operate(num1, num2, operator){
     switch (operator){
-        case "add":
+        case "+":
             return add(num1, num2);
-        case "subtract":
+        case "-":
             return subtract(num1, num2);
-        case "multiply":
+        case "*":
             return multiply(num1, num2);
-        case "divide":
+        case "/":
             return divide(num1, num2);
     }
 }
@@ -48,12 +44,15 @@ function operate(num1, num2, operator){
 function button_push(event){
     // Parent divs that hold the buttons actually have the info
     parent_classes = event.target.parentElement.classList;
+    console.log(event.target.parentElement.classList);
+    console.log(event.target.parentElement.id);
     parent_id = event.target.parentElement.id;
-
+    
     // If it's a number button
     if (parent_classes.contains("number")){
         send_additional_number_to_display(event.target.textContent);
     }
+
     //If it's .
     else if (parent_id == "period"){
         send_decimal_to_display();
@@ -62,22 +61,12 @@ function button_push(event){
     else if (parent_id == "clear"){
         clear_display();
     }
-    else if (parent_classes.contains("operator")){
-        operator_pushed(parent_id);
-    }
-
-    else if (parent_id == "equals"){
-        equals_pushed();
-    }
-
-    
 }
 
 
 function send_additional_number_to_display(num){
-    if ((display.textContent=="0") || (clear_display_on_next_button_pushed == 1)) {
+    if (display.textContent=="0") {
         display.textContent = num;
-        clear_display_on_next_button_pushed = 0;
     }
     else {
         if (display.textContent.length < 9){
@@ -94,25 +83,4 @@ function send_decimal_to_display(){
 
 function clear_display(){
     display.textContent = "";
-}
-
-function operator_pushed(pushed_operator){
-    if (isNaN(number1)){
-        number1 = display.textContent;
-    }
-
-    operator = pushed_operator;
-
-    clear_display_on_next_button_pushed = 1;
-
-}
-
-function equals_pushed(){
-    number2 = display.textContent;
-    display.textContent = operate(Number(number1), Number(number2), operator);
-}
-
-function set_numbers_to_NaN(){
-    number1 = NaN;
-    number2 = NaN;
 }
