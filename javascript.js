818fa0f8bcc3b5f18e4d0461b6d3bf14c3c7ca98
Bergmann, Erik - BLS <bergmann.erik@bls.gov>
2025-06-16 09:55:47 -0400

    Add equals capability
    Basics are there for standard operators. Still need to add in chaining operations and resetting global variables.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,10 @@
 // Globals
 var body = document.querySelector("body");
 var display = document.querySelector("#display")
+var number1 = NaN;
+var number2 = NaN;
+var operator = "add";
+var clear_display_on_next_button_pushed = 0
 
 // Attach Number functions to buttons
 var buttons = document.querySelectorAll("button")
@@ -30,13 +34,13 @@ function divide(num1, num2){
 
 function operate(num1, num2, operator){
     switch (operator){
-        case "+":
+        case "add":
             return add(num1, num2);
-        case "-":
+        case "subtract":
             return subtract(num1, num2);
-        case "*":
+        case "multiply":
             return multiply(num1, num2);
-        case "/":
+        case "divide":
             return divide(num1, num2);
     }
 }
@@ -44,15 +48,12 @@ function operate(num1, num2, operator){
 function button_push(event){
     // Parent divs that hold the buttons actually have the info
     parent_classes = event.target.parentElement.classList;
-    console.log(event.target.parentElement.classList);
-    console.log(event.target.parentElement.id);
     parent_id = event.target.parentElement.id;
-    
+
     // If it's a number button
     if (parent_classes.contains("number")){
         send_additional_number_to_display(event.target.textContent);
     }
-
     //If it's .
     else if (parent_id == "period"){
         send_decimal_to_display();
@@ -61,12 +62,22 @@ function button_push(event){
     else if (parent_id == "clear"){
         clear_display();
     }
+    else if (parent_classes.contains("operator")){
+        operator_pushed(parent_id);
+    }
+
+    else if (parent_id == "equals"){
+        equals_pushed();
+    }
+
+    
 }
 
 
 function send_additional_number_to_display(num){
-    if (display.textContent=="0") {
+    if ((display.textContent=="0") || (clear_display_on_next_button_pushed == 1)) {
         display.textContent = num;
+        clear_display_on_next_button_pushed = 0;
     }
     else {
         if (display.textContent.length < 9){
@@ -83,4 +94,25 @@ function send_decimal_to_display(){
 
 function clear_display(){
     display.textContent = "";
+}
+
+function operator_pushed(pushed_operator){
+    if (isNaN(number1)){
+        number1 = display.textContent;
+    }
+
+    operator = pushed_operator;
+
+    clear_display_on_next_button_pushed = 1;
+
+}
+
+function equals_pushed(){
+    number2 = display.textContent;
+    display.textContent = operate(Number(number1), Number(number2), operator);
+}
+
+function set_numbers_to_NaN(){
+    number1 = NaN;
+    number2 = NaN;
 }
